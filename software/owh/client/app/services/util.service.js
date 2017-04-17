950657e63299bef0b92e24ae9cba9ec37205f8f8
Joseph Jones <joseph.jones@semanticbits.com>
2017-04-17 11:15:30 -0400

    OWH-920: cleanup coe

--- software/owh/client/app/services/util.service.js
+++ software/owh/client/app/services/util.service.js
@@ -508,7 +508,7 @@
                             return;
                         }
                         var questionCellAdded = false;
-                        angular.forEach(eachData, function(eachPramsData, eachDataIndex) {
+                        angular.forEach(eachData, function(eachPramsData) {
                             var childTableData = prepareMixedTableRowData(rowHeaders.slice(1), columnHeaders, eachPramsData, countKey, totalCount, calculatePercentage, calculateRowTotal, secondaryCountKeys);
                             if(rowHeaders.length > 1 && calculateRowTotal) {
                                 childTableData.push(prepareTotalRow(eachPramsData, countKey, childTableData[0].length, totalCount, secondaryCountKeys));
@@ -522,9 +522,6 @@
                                     color: '#833eb0'
                                 }
                             };
-                            // if(eachDataIndex < eachData.length - 1) {
-                            //     responseCell.style['border-bottom'] = 'white';
-                            // }
                             if(!questionCellAdded) {
                                 var eachTableRow = {
                                     title: matchedOption.title,
