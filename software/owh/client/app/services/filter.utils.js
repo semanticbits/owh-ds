eea343e73cc474a36a89e7a47cb3435a3933ad91
Ashok Dongare <ashok.dongare@semanticbits.com>
2017-05-18 17:15:41 -0400

    OWH-1182 update infant mortality etl

--- software/owh/client/app/services/filter.utils.js
+++ software/owh/client/app/services/filter.utils.js
@@ -88,7 +88,7 @@
             { "key": "Mexican", "title": "Mexican" },
             { "key": "Puerto Rican", "title": "Puerto Rican" },
             { "key": "Cuban", "title": "Cuban" },
-            { "key": "Central and South American", "title": "Central and South American" },
+            { "key": "Central or South American", "title": "Central or South American" },
             { "key": "Other and Unknown Hispanic", "title": "Other and Unknown Hispanic" },
             { "key": "Non-Hispanic White", "title": "Non-Hispanic White" },
             { "key": "Non-Hispanic Black", "title": "Non-Hispanic Black" },
@@ -144,7 +144,7 @@
             { "key": "25-29 years", "title": "25-29 years" },
             { "key": "30-34 years", "title": "30-34 years" },
             { "key": "35-39 years", "title": "35-39 years" },
-            { "key": "40-44 year", "title": "40-44 year" },
+            { "key": "40-44 years", "title": "40-44 years" },
             { "key": "45-49 years", "title": "45-49 years" },
             { "key": "50-54 years", "title": "50 years and over" }
         ];
