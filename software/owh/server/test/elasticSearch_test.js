aa38535574e6afb45f41ab351d0a3c74715baa4f
JanakiRam <janakiram.gollapudi@semanticbits.com>
2017-07-10 12:19:28 -0400

    moved test.js test cases into elasticsearch_test.js

--- software/owh/server/test/elasticSearch_test.js
+++ software/owh/server/test/elasticSearch_test.js
@@ -616,4 +616,15 @@ describe("Elastic Search", function () {
         })
     });
 
+    it('should merge populations into mortality response', function(done){
+        var mort =   {"data":{"nested":{"table":{"group_table_race":[{"name":"1","deaths":2106697,"undefined":[{"name":"F","deaths":1079109},{"name":"M","deaths":1027588}]},{"name":"2","deaths":291706,"undefined":[{"name":"M","deaths":148258},{"name":"F","deaths":143448}]}]}}}};
+        var census = {"data":{"nested":{"table":{"group_table_race":[{"name":"2","undefined":[{"name":"F","pop":4444},{"name":"M","pop":3333}]},{"name":"1","undefined":[{"name":"M","pop":5555},{"name":"F","pop":6666}]}]}}}};
+        var mergedData = {"data":{"nested":{"table":{"group_table_race":[{"name":"1","deaths":2106697,"undefined":[{"name":"F","deaths":1079109, "pop":6666},{"name":"M","deaths":1027588,"pop":5555 }]},{"name":"2","deaths":291706,"undefined":[{"name":"F","deaths":143448, "pop":4444},{"name":"M","deaths":148258, "pop":3333}]}]}}}};
+        var es = new elasticSearch();
+
+        es.mergeWithCensusData(mort, census, 'pop');
+        expect(JSON.stringify(mort)).equal(JSON.stringify(mergedData));
+        done();
+    });
+
 });
