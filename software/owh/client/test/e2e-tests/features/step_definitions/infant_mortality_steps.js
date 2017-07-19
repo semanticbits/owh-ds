e3ef2140d4442b3900493d0df66f70b50ef28013
Ashok Dongare <ashok.dongare@semanticbits.com>
2017-07-19 09:57:50 -0400

    Fixed test case

--- software/owh/client/test/e2e-tests/features/step_definitions/infant_mortality_steps.js
+++ software/owh/client/test/e2e-tests/features/step_definitions/infant_mortality_steps.js
@@ -62,6 +62,12 @@ var infantMortalityStepDefinitions = function () {
         }).then(next);
     });
 
+    this.Then(/^I see label "([^"]*)" and "([^"]*)" are displayed on minimized visualization"$/, function (arg1, arg2) {
+        var labelArray = imp.getAxisLabelsForMinimizedVisualization(0,0);
+        expect(labelArray[0].getText()).to.eventually.equal(arg1);
+        return expect(labelArray[1].getText()).to.eventually.equal(arg2);
+    });
+
     this.Then(/^I see the disabled options "([^"]*)" for "([^"]*)"$/, function (disabled_options, filter, next) {
        var expected = disabled_options.split('|');
        imp.getDisabledFilterOptions(filter).then(function (options) {
