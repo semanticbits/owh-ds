5804d5fb88c0561d56ff20cfc377f6a5a6a538db
JanakiRam <janakiram.gollapudi@semanticbits.com>
2017-08-03 11:42:23 -0400

    fixed common cucumber tests which is failing on travis ci

--- software/owh/client/test/e2e-tests/features/step_definitions/homeSteps.js
+++ software/owh/client/test/e2e-tests/features/step_definitions/homeSteps.js
@@ -106,7 +106,7 @@ var homeStepDefinitionsWrapper = function () {
     });
 
     this.Then(/^I see "([^"]*)" msg on footer$/, function (arg1) {
-        return expect(element(by.className("hig-formerly-msg")).getText()).to.eventually.contains(arg1);
+        return expect(element(by.className("hig-formerly-msg")).element(by.tagName('span')).getText()).to.eventually.contains(arg1);
     });
 };
 module.exports = homeStepDefinitionsWrapper;
