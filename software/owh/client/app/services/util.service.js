043fd85a74fd1aef4edfeb692b473fe917590721
Ashok Dongare <ashok.dongare@semanticbits.com>
2017-09-14 12:16:45 -0400

    OWH-2062 Age filters should be disabled across all years with Congenital Syphilis disease selected

--- software/owh/client/app/services/util.service.js
+++ software/owh/client/app/services/util.service.js
@@ -1089,7 +1089,7 @@
                     if(['2000', '2001', '2002'].indexOf(filterValue) >= 0) {
                         earlyLatentSyphilis.disabled = true;
                     }
-                    enableOrDisableFilterOptions(sideFilters, ['sex', 'race'], diseaseSideFilter.filters.value === 'Congenital Syphilis');
+                    enableOrDisableFilterOptions(sideFilters, ['sex', 'race', 'age_group'], diseaseSideFilter.filters.value === 'Congenital Syphilis');
                 }, function (error) {
                     angular.element(document.getElementById('spindiv')).addClass('ng-hide');
                     console.log(error);
