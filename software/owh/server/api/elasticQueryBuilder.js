e5502a4d62fcaf7e54efef185c5a546420b97745
JanakiRam <janakiram.gollapudi@semanticbits.com>
2017-09-16 15:01:52 -0400

    fixed suppression applying if user select 'National' for disease datasets

--- software/owh/server/api/elasticQueryBuilder.js
+++ software/owh/server/api/elasticQueryBuilder.js
@@ -376,7 +376,7 @@ function findFilterByKeyAndValue(a, key, value) {
  */
 function isFilterApplied(a) {
     if (a) {
-        return a.value.length > 0 || a.groupBy;
+        return (a.value.length > 0 && a.value != 'National') || a.groupBy;
     }
     return false;
 }
