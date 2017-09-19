1e7f3addfcd20e65b3e31c252a3492c098ac693b
edefikh <defikh@gmail.com>
2017-09-19 17:00:06 +0300

    modify travis.yml

---
+++ script.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+for  i in `aws ecs list-tasks --cluster owh-prod --service owh-prod-frontend-service --region us-east-1|grep arn|tr -d ","|tr -d '"'|awk -F/ '{print $2}'`
+do 
+aws ecs stop-task --task $i --cluster owh-prod --region us-east-1
+done
