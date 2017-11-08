037df5456c57bb916abfbe931aff8556472c15f8
Ashok Dongare <ashok.dongare@semanticbits.com>
2017-11-08 14:07:17 -0500

    OWH-2164 User must be able to click on Hide Percentages and Off toggle buttons as expected

--- software/owh/client/app/modules/search/search.controller.js
+++ software/owh/client/app/modules/search/search.controller.js
@@ -752,7 +752,7 @@
                     sc.mapPopup
                         .setContent(compileEle[0])
                         .setLatLng(L.latLng(lat, lng)).openOn(map);
-                }, 5);
+                }, 100);
             } else {
                 sc.mapPopup
                     .setLatLng(L.latLng(lat, lng));
