7584997c30aa63094641266323867b1c09058729
JanakiRam <janakiram.gollapudi@semanticbits.com>
2017-11-10 13:48:03 -0500

    fixed issues with map tooltip sometimes showing empty

--- software/owh/client/app/modules/search/search.controller.js
+++ software/owh/client/app/modules/search/search.controller.js
@@ -802,6 +802,8 @@
             map.invalidateSize();
             map.eachLayer(function (layer){
                 if(layer.feature) {
+                    layer.off("mouseover");
+                    layer.off("mouseout");
                     layer.on("mouseover", function (event) {
                         if(sc.filters.selectedPrimaryFilter && event.target.feature) {
                             buildMarkerPopup(event.latlng.lat, event.latlng.lng, event.target.feature.properties,
