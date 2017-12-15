4318e214a498017045be4e76da1b7782dae7d81a
JanakiRam <janakiram.gollapudi@semanticbits.com>
2017-12-14 19:44:56 -0500

    added full path for 'docker-compose' command

--- deployment/scripts/kill_container.sh
+++ deployment/scripts/kill_container.sh
@@ -7,7 +7,7 @@ echo "- Executing in /tmp/deploy -"
 ls
 pwd
 echo "- docker compose down - "
-docker-compose down
+/usr/local/bin/docker-compose down
 echo " - removing images -"
 docker rmi owh_web
 echo "***completed***"
--- deployment/scripts/start-compose.sh
+++ deployment/scripts/start-compose.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker-compose up -d
+/usr/local/bin/docker-compose up -d
