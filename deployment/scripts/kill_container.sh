63e9f40406e17ad6e9262ecaf182d88d9e3c6637
JanakiRam <janakiram.gollapudi@semanticbits.com>
2017-12-14 20:02:03 -0500

    added echo statements to verify docker-compose version

--- deployment/scripts/kill_container.sh
+++ deployment/scripts/kill_container.sh
@@ -6,7 +6,8 @@ cd /tmp/deploy
 echo "- Executing in /tmp/deploy -"
 ls
 pwd
-echo "- docker compose down - "
+echo "- /usr/local/bin/docker-compose down - "
+/usr/local/bin/docker-compose version
 /usr/local/bin/docker-compose down
 echo " - removing images -"
 docker rmi owh_web
