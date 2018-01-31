bf4c13b82e122528c2bab4e1bd37b67fefdf58d5
adongare <ashok.dongare@semanticbits.com>
2018-01-31 10:53:12 -0500

    Addressed OWH-2231 reviews

--- software/owh/client/app/app.run.js
+++ software/owh/client/app/app.run.js
@@ -420,16 +420,16 @@
         API.getGoogleAnalyticsInfo().$promise.then(function(response){
             // initialise google analytics
             $window.ga('create', response.data.trackingID, 'auto');
-            var routs = {deaths:'Detailed Mortality', natality:"Natality", bridge_race:"Bridged Race",
+            var routes = {deaths:'Detailed Mortality', natality:"Natality", bridge_race:"Bridged Race",
                 mental_health: 'YRBSS', 'brfss': 'BRFSS', prams:"PRAMS", cancer_incident: 'Cancer Incidence',
                 cancer_mortality:'Cancer Mortality', infant_mortality:'Infant Mortality', tb: 'TB', std: 'STD',
-                aids:'HIV/AIDS', factsheets: 'Factsheets', home: 'Home', description:'Data Documentation'};
+                aids:'HIV/AIDS', factsheets: 'Fact Sheets', home: 'Home', description:'Data Documentation'};
             $rootScope.$on('$stateChangeSuccess', function (event) {
                 var url = $location.path();
                 if($stateParams.queryID && $stateParams.queryID != '' && $stateParams.primaryFilterKey) {
-                    url = url + '/?dataset='+ routs[$stateParams.primaryFilterKey];
+                    url = url + '/?dataset='+ routes[$stateParams.primaryFilterKey];
                 } else {
-                    url = url + '/?dataset='+routs[$state.current.name];
+                    url = url + '/?dataset='+routes[$state.current.name];
                 }
                 $window.ga('send', 'pageview', url);
             });
